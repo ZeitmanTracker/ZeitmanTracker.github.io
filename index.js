4b869efa21515eeb6b99986622f2c403b1c948ee
ZeitmanTracker <128743229+ZeitmanTracker@users.noreply.github.com>
2023-04-09 23:16:12 -0400

    The vote update :)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // CONFIG =========================
-// API_URL = 'http://localhost' // DEVELOPMENT
-API_URL = 'https://zietman-tracker.ziettracker.repl.co' // PRODUCTION
+API_URL = 'http://localhost' // DEVELOPMENT
+// API_URL = 'https://zietman-tracker.ziettracker.repl.co' // PRODUCTION
 
 // ================================
 
@@ -22,9 +22,11 @@ if (window.matchMedia('(display-mode: standalone)').matches){
 mouseX = 0;
 mouseY = 0;
 
+voteTeacherName = "Unknown Teacher"
+
 teacherPinLocation = {
     'zeitman': 'office',
-    'holey':   'office',
+    'vote_teacher':   'office',
 }
 
 String.prototype.capFirst = function() {return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });};
@@ -82,16 +84,20 @@ getReportChoice = (location) => {
         }, 1000)
       },
       () => {
+        if (voteTeacherName.toLowerCase().startsWith('none')){
+            alertify.error('No teacher selected yet! Come back at Mon. 10:30 AM');
+            return;
+        }
         setTimeout(() => {
-            confirmThen('Are you sure?', 'Are you sure you want to report Holey at the ' + location.replaceAll("_"," ").capFirst() + '?', () => {
-              sendReport(location, 'holey');
+            confirmThen('Are you sure?', `Are you sure you want to report ${voteTeacherName} at the ` + location.replaceAll("_"," ").capFirst() + '?', () => {
+              sendReport(location, 'vote_teacher');
             });
         }, 1000)
       })
         .setting({
           'labels': {
             ok: 'Zeitman',
-            cancel: 'Holey'
+            cancel: voteTeacherName
           },
           'title': 'Submit a report for ' + location.replaceAll("_", " ").capFirst() + '?',
           'invokeOnCloseOff': true, // so we can close the dialog without invoking the callback
@@ -142,7 +148,7 @@ sendReport = (location, teacher) => {
         method: 'POST'
     }).then((response) => {
         if (response.status == 200) {
-            alertify.success("Report submitted!");
+            alertify.success("Zeiting submitted!");
             redrawMap()
         } else {
             alertify.error("Error submitting report!");
@@ -168,6 +174,8 @@ redrawMap = () => {
                     if (snapLocations.hasOwnProperty(key)) {
                         ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
                         extraWidth = key.capFirst().length * 2.8;
+                        extraWidth2 = voteTeacherName.length * 2.8;
+                        extraWidth = Math.max(extraWidth, extraWidth2);
                         ctx.fillRect(snapLocations[key][0], snapLocations[key][1], boxSize[0] + extraWidth, boxSize[1] + (Object.keys(data[key]).length * 8));
 
                         ctx.fillStyle = 'white';
@@ -178,20 +186,24 @@ redrawMap = () => {
                         ctx.font = '14px Arial';
                         yMod = 0;
                         for (teacher in data[key]){
-                            ctx.fillText(`${teacher} - ${data[key][teacher]}`, snapLocations[key][0] + 5, snapLocations[key][1] + 35 + yMod);
+                            nme = teacher
+                            if (teacher == 'vote_teacher') {
+                                nme = voteTeacherName
+                            }
+                            ctx.fillText(`${nme.capFirst()} - ${data[key][teacher]}`, snapLocations[key][0] + 5, snapLocations[key][1] + 35 + yMod);
                             yMod += 15;
                         }
                     }
                 }
 
-                holeyPin = new Image();
-                holeyPin.src = '/images/holey.png';
-                holeyPin.onload = () => {
+                vote_teacherPin = new Image();
+                vote_teacherPin.src = '/images/vote_teacher.png';
+                vote_teacherPin.onload = () => {
                     // ctx.drawImage(zeitmanPin, pinX, pinY - 25, 30, 30);
                     zeitmanLocation = snapLocations[teacherPinLocation['zeitman']]
                     ctx.drawImage(zeitmanPin, zeitmanLocation[0], zeitmanLocation[1] - 25, 30, 30);
-                    holeyLocation = snapLocations[teacherPinLocation['holey']]
-                    ctx.drawImage(holeyPin, holeyLocation[0] + 40, holeyLocation[1] - 25, 30, 30);
+                    vote_teacherLocation = snapLocations[teacherPinLocation['vote_teacher']]
+                    ctx.drawImage(vote_teacherPin, vote_teacherLocation[0] + 40, vote_teacherLocation[1] - 25, 30, 30);
     
                     ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
                     ctx.fillRect(0, 0, 300, 30);
@@ -213,6 +225,103 @@ redrawMap = () => {
     document.getElementsByClassName('map')[0].width = img.width;
     document.getElementsByClassName('map')[0].height = img.height;
 }
+
+// votemenu code ================================================================
+updateStandings = () => {
+    fetch(`${API_URL}/getstandings`).then((response) => {
+        return response.json();
+    }).then((data) => {
+        firstPlace  = document.getElementsByClassName('current-firstplace')[0];
+        secondPlace = document.getElementsByClassName('current-secondplace')[0];
+        thirdPlace  = document.getElementsByClassName('current-thirdplace')[0];
+
+        // find the first and second place
+        firstPlaceName  = 'None'; firstPlaceVotes  = 0;
+        secondPlaceName = 'None'; secondPlaceVotes = 0;
+        thirdPlaceName  = 'None'; thirdPlaceVotes  = 0;
+
+        for (var key in data){
+            if (data[key] > firstPlaceVotes) {
+                thirdPlaceName  = secondPlaceName;
+                thirdPlaceVotes = secondPlaceVotes;
+                secondPlaceName = firstPlaceName;
+                secondPlaceVotes = firstPlaceVotes;
+                firstPlaceName  = key;
+                firstPlaceVotes = data[key];
+            } else if (data[key] > secondPlaceVotes) {
+                thirdPlaceName  = secondPlaceName;
+                thirdPlaceVotes = secondPlaceVotes;
+                secondPlaceName = key;
+                secondPlaceVotes = data[key];
+            } else if (data[key] > thirdPlaceVotes) {
+                thirdPlaceName  = key;
+                thirdPlaceVotes = data[key];
+            }
+        }
+
+        firstPlace.innerHTML  = `${firstPlaceName.capFirst()} - ${firstPlaceVotes}`;
+        secondPlace.innerHTML = `${secondPlaceName.capFirst()} - ${secondPlaceVotes}`;
+        thirdPlace.innerHTML  = `${thirdPlaceName.capFirst()} - ${thirdPlaceVotes}`;
+    });
+}
+
+document.getElementsByClassName('votebutton')[0].addEventListener('click', () => {
+    document.getElementsByClassName('votemenu')[0].style.display = 'block';
+    updateStandings();
+})
+
+document.getElementsByClassName('closebutton')[0].addEventListener('click', () => {
+    document.getElementsByClassName('votemenu')[0].style.display = 'none';
+})
+
+document.getElementsByClassName('votesubmitbutton')[0].addEventListener('click', () => {
+    alertify.confirm('Are you sure you want to vote for ' + document.getElementById('teachers').value.capFirst() + '?', () => {
+        // check for last vote time
+        var lastVoteTime = document.cookie.replace(/(?:(?:^|.*;\s*)last_vote_time\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        if (lastVoteTime != '') {
+            if (Date.now() - lastVoteTime < 1000 * 60 * 60 * 24) {
+                alertify.error("You can only vote once per day!");
+                return;
+            }
+        }
+    
+        var d = new Date();
+        d.setTime(d.getTime() + (1*24*60*60*1000)); // 1 day
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = "last_vote_time=" + Date.now() + ";" + expires + ";path=/";
+    
+        selected = document.getElementById('teachers').value;
+        fetch(`${API_URL}/castvote/${selected}`).then((response) => {
+            if (response.status == 200) {
+                alertify.success("Vote submitted!");
+                setTimeout(() => {
+                    updateStandings();
+                }, 1000)
+            } else {
+                alertify.error("Error submitting vote!");
+            }
+        });
+    }).set('title', 'Confirm Vote');
+})
+
+fetch(`${API_URL}/teacherlist`).then((response) => {
+    return response.json();
+}).then((data) => {
+    teacherList = data;
+    teacherSelect = document.getElementById('teachers');
+    for (var key in teacherList['teachers']){
+        teacherSelect.innerHTML += `<option value="${teacherList['teachers'][key]}">${teacherList['teachers'][key].capFirst()}</option>`;
+    }
+});
+
+// ===============================================================================
+
+fetch(`${API_URL}/getvoteteacher`).then((response) => {
+    return response.text();
+}).then((data) => {
+    voteTeacherName = data;
+    document.getElementById('voteteacherdisp').innerHTML = document.getElementById('voteteacherdisp').innerHTML.replace('??VOTE_TEACHER??', voteTeacherName.capFirst());
+});
 
 // load the image into the canvas
 ctx = document.getElementsByClassName('map')[0].getContext('2d');
@@ -239,8 +348,8 @@ locationChecker = () => {
             teacherPinLocation['zeitman'] = data['zeitman'];
             redrawMap();
         }
-        if (data['holey'] != teacherPinLocation['holey']){
-            teacherPinLocation['holey'] = data['holey'];
+        if (data['vote_teacher'] != teacherPinLocation['vote_teacher']){
+            teacherPinLocation['vote_teacher'] = data['vote_teacher'];
             redrawMap();
         }
     })
